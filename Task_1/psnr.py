f92fc380991d10ab1668d30a56432b50c1839d89
ErmolinDA <47722197+ErmolinDA@users.noreply.github.com>
2020-10-18 23:07:43 +0300

    add PSNR stable

--- Task_1/psnr.py
+++ Task_1/psnr.py
@@ -7,13 +7,20 @@ def psnr(first, second):
     if first.shape != second.shape:
         print('Image size mismatch')
         return 0
+    first = first.astype(np.float64) / 255.
+    second = second.astype(np.float64) / 255.
     mse = np.mean((first - second) ** 2)
     if mse == 0:
         return 100
-    max_pix = 255
-    return 10 * math.log10(max_pix ** 2 / mse)
+    return 10 * math.log10(1.0 ** 2 / mse)
 
 
-img = cv2.imread("boka.jpg", 1)
-img2 = cv2.imread("joka.jpg", 1)
+img = cv2.imread("joka.jpg", 1)
+img2 = cv2.imread("joka_feat_noize.jpg", 1)
+print(f'CV2 gray: {cv2.PSNR(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY),255)}')
+print(f'Pair similarity gray: {psnr(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY))}')
+print(f'CV2: {cv2.PSNR(img, img2,255)}')
 print(f'Pair similarity: {psnr(img, img2)}')
+cv2.imshow("Original", img)
+cv2.imshow("Compressed", img)
+cv2.waitKey()
